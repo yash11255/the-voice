677a07ee7421de19af5dad4c143ffcf49ba62e59
Darshan Singh <69538056+yash11255@users.noreply.github.com>
2024-12-18 14:15:09 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,53 @@
+// import React from 'react';
+// import { NextPageContext } from 'next';
+// import { getSession } from 'next-auth/react';
+
+// import Navbar from '@/components/Navbar';
+// import Billboard from '@/components/Billboard';
+// import MovieList from '@/components/MovieList';
+// import InfoModal from '@/components/InfoModal';
+// import useMovieList from '@/hooks/useMovieList';
+// import useFavorites from '@/hooks/useFavorites';
+// import useInfoModalStore from '@/hooks/useInfoModalStore';
+
+// export async function getServerSideProps(context: NextPageContext) {
+//   const session = await getSession(context);
+
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/auth',
+//         permanent: false,
+//       }
+//     }
+//   }
+
+//   return {
+//     props: {}
+//   }
+// }
+
+// const Home = () => {
+//   const { data: movies = [] } = useMovieList();
+//   const { data: favorites = [] } = useFavorites();
+//   const {isOpen, closeModal} = useInfoModalStore();
+
+//   return (
+//     <>
+//       <InfoModal visible={isOpen} onClose={closeModal} />
+//       <Navbar />
+//       <Billboard />
+//       <div className="pb-40">
+//         <MovieList title="Trending Now" data={movies} />
+//         <MovieList title="My List" data={favorites} />
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Home;
+
 import React from 'react';
-import { NextPageContext } from 'next';
-import { getSession } from 'next-auth/react';
 
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
@@ -10,39 +57,33 @@ import useMovieList from '@/hooks/useMovieList';
 import useFavorites from '@/hooks/useFavorites';
 import useInfoModalStore from '@/hooks/useInfoModalStore';
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      }
-    }
-  }
-
-  return {
-    props: {}
-  }
-}
-
 const Home = () => {
+  // Fetch movie data and favorites using custom hooks
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
-  const {isOpen, closeModal} = useInfoModalStore();
+  const { isOpen, closeModal } = useInfoModalStore();
 
   return (
     <>
+      {/* InfoModal to display additional movie details */}
       <InfoModal visible={isOpen} onClose={closeModal} />
+      
+      {/* Navigation bar */}
       <Navbar />
+      
+      {/* Featured billboard section */}
       <Billboard />
+      
+      {/* Movie lists */}
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
-        <MovieList title="My List" data={favorites} />
+        {favorites.length > 0 && (
+          <MovieList title="My List" data={favorites} />
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
+
