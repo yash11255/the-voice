10818e00530f2ee4ca533577cf71aee9c102a6f4
Darshan Singh <69538056+yash11255@users.noreply.github.com>
2024-12-18 14:22:01 +0530

    Update useCurrentUser.ts

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,15 +1,30 @@
-import useSwr from 'swr'
+// import useSwr from 'swr'
 
-import fetcher from '@/libs/fetcher';
+// import fetcher from '@/libs/fetcher';
+
+// const useCurrentUser = () => {
+//   const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+//   return {
+//     data,
+//     error,
+//     isLoading,
+//     mutate,
+//   }
+// };
+
+// export default useCurrentUser;
+import useSWR from 'swr';
+import fetcher from '@/libs/fetcher';  // Assuming fetcher is still used
 
 const useCurrentUser = () => {
-  const { data, error, isLoading, mutate } = useSwr('/api/current', fetcher);
+  const { data, error, isLoading, mutate } = useSWR('/api/current', fetcher);
+
   return {
     data,
     error,
     isLoading,
     mutate,
-  }
+  };
 };
 
 export default useCurrentUser;
