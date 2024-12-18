f0a04681ac2d6669cbd47b64e67e8e488c043a36
Darshan Singh <69538056+yash11255@users.noreply.github.com>
2024-12-18 14:22:20 +0530

    Update useMovie.ts

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,17 +1,40 @@
-import useSwr from 'swr'
+// import useSwr from 'swr'
+// import fetcher from '@/libs/fetcher';
+
+// const useMovie = (id?: string) => {
+//   const { data, error, isLoading } = useSwr(id ? `/api/movies/${id}` : null, fetcher, {
+//     revalidateIfStale: false,
+//     revalidateOnFocus: false,
+//     revalidateOnReconnect: false,
+//   });
+//   return {
+//     data,
+//     error,
+//     isLoading
+//   }
+// };
+
+// export default useMovie;
+import useSwr from 'swr';
 import fetcher from '@/libs/fetcher';
 
 const useMovie = (id?: string) => {
-  const { data, error, isLoading } = useSwr(id ? `/api/movies/${id}` : null, fetcher, {
-    revalidateIfStale: false,
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-  });
+  const { data = {}, error, isLoading } = useSwr(
+    id ? `/api/movies/${id}` : null, // Fetch only if id is provided
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
   return {
-    data,
-    error,
-    isLoading
-  }
+    data,         // Movie data (defaults to an empty object)
+    error,        // Error object (if any)
+    isLoading,    // Loading state
+    hasError: !!error, // Helper boolean to check for errors
+  };
 };
 
 export default useMovie;
