545e2aa697a7efe0018620d96e253fdc2d49c68a
Darshan Singh <69538056+yash11255@users.noreply.github.com>
2024-12-18 15:02:33 +0530

    Update fetcher.ts

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -3,6 +3,11 @@
 // const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 // export default fetcher;
-const fetcher = (url) => fetch(url).then((res) => res.json());  // No authentication required
+// const fetcher = (url) => fetch(url).then((res) => res.json());  // No authentication required
+
+// export default fetcher;
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());  // Specify type for url
 
 export default fetcher;
+
