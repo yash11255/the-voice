b8f8b7fa7cd41300ca7a375a0743b6ff2edecd4f
Darshan Singh <69538056+yash11255@users.noreply.github.com>
2024-12-18 14:22:39 +0530

    Update useMovieList.ts

--- hooks/useMovieList.ts
+++ hooks/useMovieList.ts
@@ -1,17 +1,36 @@
-import useSwr from 'swr'
+// import useSwr from 'swr'
+// import fetcher from '@/libs/fetcher';
+
+// const useMovies = () => {
+//   const { data, error, isLoading } = useSwr('/api/movies', fetcher, {
+//     revalidateIfStale: false,
+//     revalidateOnFocus: false,
+//     revalidateOnReconnect: false,
+//   });
+//   return {
+//     data,
+//     error,
+//     isLoading
+//   }
+// };
+
+// export default useMovies;
+import useSwr from 'swr';
 import fetcher from '@/libs/fetcher';
 
-const useMovies = () => {
-  const { data, error, isLoading } = useSwr('/api/movies', fetcher, {
+const useMovieList = () => {
+  const { data = [], error, isLoading } = useSwr('/api/movies', fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
+
   return {
-    data,
+    data, // Return movies (default to an empty array)
     error,
-    isLoading
-  }
+    isLoading,
+    hasError: !!error, // Helper boolean for error checking
+  };
 };
 
-export default useMovies;
+export default useMovieList;
